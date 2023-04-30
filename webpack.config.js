36abecd8207942611454ce6d23fcf0b8db29789a
Inderprit Singh <singhinderprit1992@gmail.com>
2023-04-29 23:00:49 -0400

    added ruleset for the html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         filename: 'main.[contenthash].js',
     },
+    module: {
+        rules: [{ test: /\.html$/, use: ['html-loader'] }],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/template.html',
@@ -18,4 +21,10 @@ module.exports = {
 /**
  * Html webpack plugin - simpflies creationg of HTML files. Useful for bundles that include a hash in the filename.
  * This will let webpack handle automatically update the hashed file names in the script tags
+ *
+ *
+ * Html loader - anytime that you need to handle an image, it will require the image. It will tell the webpack that this
+ * is the file that we need to load. You still need to tell webpack how you want to handle these files.
+ *
+ *
  */
