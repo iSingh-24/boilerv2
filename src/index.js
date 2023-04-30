1123ecbfb884fe7a9893229871b7a9bf3f29d2d7
Inderprit Singh <singhinderprit1992@gmail.com>
2023-04-30 03:43:22 -0400

    added webpack server to dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /**
- * TODO: update package json run scripts and install the dependencies needed
  * TODO: add a means to read babel and finish setting up client-side
  * TODO: set up server-side related tasks
  *
