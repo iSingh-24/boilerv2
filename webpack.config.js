aab66f9fc45fe6ec6b3c80d09ffe6db364409ed8
Inderprit Singh <singhinderprit1992@gmail.com>
2023-04-29 22:48:29 -0400

    added template file to src for html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -7,4 +8,14 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         filename: 'main.[contenthash].js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
 };
+
+/**
+ * Html webpack plugin - simpflies creationg of HTML files. Useful for bundles that include a hash in the filename.
+ * This will let webpack handle automatically update the hashed file names in the script tags
+ */
