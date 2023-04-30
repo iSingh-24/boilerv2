da279070454ef4f38f00bc0965b84990a14d5152
Inderprit Singh <singhinderprit1992@gmail.com>
2023-04-30 00:56:55 -0400

    set up a TODO

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+/**
+ * TODO: update package json run scripts and install the dependencies needed
+ * TODO: add in the css module reader including sass
+ * TODO: add a means to read babel and finish setting up client-side
+ * TODO: set up server-side related tasks
+ *
+ * OPTIONAL: Read more about diff webpack config files
+ */
+
 console.log('helloo');
