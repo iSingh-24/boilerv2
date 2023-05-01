3ad5eda67cc30c3a9b04f04fea8f233368eb62c9
Inderprit Singh <singhinderprit1992@gmail.com>
2023-04-30 20:19:52 -0400

    updated path.join to path.resolve, also added morgan and express json and urlencoded

--- server/index.js
+++ server/index.js
@@ -3,10 +3,20 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../dist')));
+// app.use(express.static(path.join(__dirname, '../dist')));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist'));
+    res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
