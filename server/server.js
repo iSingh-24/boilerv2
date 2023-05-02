db9f76168aea5611f5d68d787b19d64c17fc3350
Inderprit Singh <singhinderprit1992@gmail.com>
2023-05-02 19:25:52 -0400

    moved around init function, changed up scripts, still needs cleanup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const { db } = require('./db/db');
 
 const app = express();
 
@@ -21,6 +23,8 @@ app.use(morgan('dev'));
  * the size of the parsed data and may not be able to handle complex nested objects. If you need to parse complex nested objects, it's better to set extended to true.
  */
 
+console.log('Hello');
+
 app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
 app.get('/', (req, res) => {
@@ -29,6 +33,19 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+const init = async () => {
+    try {
+        await db.sync({ force: true }); //typically we don't want to do force true in production mode.
+        // await User.create({ firstName: 'John' });
+
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`);
+        });
+
+        console.log('connected');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+init();
